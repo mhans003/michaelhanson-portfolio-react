3ac2ba06bfe9e21835405d89a1fb070f0c527f1f
mhans003 <michaeledwardhanson@gmail.com>
2021-01-01 15:18:50 -0800

    Resume page now allows user to download a resume copy and view it embedded on the page.

---
+++ src/components/ResumeEmbed/index.js
@@ -0,0 +1,9 @@
+function ResumeEmbed() {
+    return (
+        <div className="main-content">
+            <embed src="./assets/MichaelHansonResume.pdf" type="application/pdf" width="100%" height="600px"/>
+        </div>
+    );
+}
+
+export default ResumeEmbed;
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,11 +1,22 @@
 import "./style.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Container from "../components/Container";
+import ResumeEmbed from "../components/ResumeEmbed";
 
 function Resume(props) {
     return (
         <div>
             <Header current={props.current}/>
+            <Container main>
+                <h1>
+                    Resume <a href="./assets/MichaelHansonResume.pdf" download>
+                        <i id="download-icon" className="fad fa-arrow-to-bottom"></i>
+                    </a>
+                </h1>
+                <hr/>
+                <ResumeEmbed/>
+            </Container>
             <Footer/>
         </div>
     );
